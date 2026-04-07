92ab387bddc2ea54e202466ffd846443ba3f638c
Jesus Rodolfo Gil farias <jrgil.20@est.ucab.edu.ve>
2026-04-07 21:42:57 +0000

    Revert a bucle para verificar la integridad de archivos a condiciones individuales

--- backup.sh
+++ backup.sh
@@ -2,8 +2,18 @@ sudo cp -r /etc "/mnt/backup/etc_backup" 2>/dev/null
 
 sudo tar -czf "/mnt/backup/var_log.tar.gz" /var/log 2>/dev/null
 
-for file in "/etc/passwd" "/etc/shadow" "/etc/sudoers" "/boot/grub/grub.cfg"; do
-    if [ -f "$file" ] || [ -d "$file" ]; then
-        sudo sha256sum "$file" >> "/mnt/backup/integrity_check.sha256" 2>/dev/null
-    fi
-done
+if [ -f "/etc/passwd" ] || [ -d "/etc/passwd" ]; then
+    sudo sha256sum "/etc/passwd" >> "/mnt/backup/integrity_check.sha256" 2>/dev/null
+fi
+
+if [ -f "/etc/shadow" ] || [ -d "/etc/shadow" ]; then
+    sudo sha256sum "/etc/shadow" >> "/mnt/backup/integrity_check.sha256" 2>/dev/null
+fi
+
+if [ -f "/etc/sudoers" ] || [ -d "/etc/sudoers" ]; then
+    sudo sha256sum "/etc/sudoers" >> "/mnt/backup/integrity_check.sha256" 2>/dev/null
+fi
+
+if [ -f "/boot/grub/grub.cfg" ] || [ -d "/boot/grub/grub.cfg" ]; then
+    sudo sha256sum "/boot/grub/grub.cfg" >> "/mnt/backup/integrity_check.sha256" 2>/dev/null
+fi
