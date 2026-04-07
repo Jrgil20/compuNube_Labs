b96799788e70ab9a5858f02faac712382a11ab25
Jesus Rodolfo Gil farias <jrgil.20@est.ucab.edu.ve>
2026-04-07 21:41:56 +0000

    Refactor script de respaldo para simplificar la verificación de integridad de archivos

--- backup.sh
+++ backup.sh
@@ -2,17 +2,8 @@ sudo cp -r /etc "/mnt/backup/etc_backup" 2>/dev/null
 
 sudo tar -czf "/mnt/backup/var_log.tar.gz" /var/log 2>/dev/null
 
-INTEGRITY_FILE="/mnt/backup/integrity_check.sha256"
-
-IMPORTANT_FILES=(
-    "/etc/passwd"
-    "/etc/shadow"
-    "/etc/sudoers"
-    "/boot/grub/grub.cfg"
-)
-
-for file in "${IMPORTANT_FILES[@]}"; do
+for file in "/etc/passwd" "/etc/shadow" "/etc/sudoers" "/boot/grub/grub.cfg"; do
     if [ -f "$file" ] || [ -d "$file" ]; then
-        sudo sha256sum "$file" >> "$INTEGRITY_FILE" 2>/dev/null
+        sudo sha256sum "$file" >> "/mnt/backup/integrity_check.sha256" 2>/dev/null
     fi
 done
